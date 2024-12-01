bfa2676b5047901f2e28d5f52d55ea5949763d65
z7pz <sdfygamer@gmail.com>
2024-12-01 08:06:01 +0300

    commit

--- app/main.ts
+++ app/main.ts
@@ -5,7 +5,8 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-  socket.on("close", () => {
+	socket.write("HTTP/1.1 200 OK\r\n\r\n")
+	socket.on("close", () => {
     socket.end();
   });
 });
