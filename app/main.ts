11ee1c98f2557e0f7e71d42149938e5c1506d1a3
z7pz <sdfygamer@gmail.com>
2024-12-01 08:48:25 +0300

    --allow-empty

--- app/main.ts
+++ app/main.ts
@@ -31,7 +31,8 @@ const server = net.createServer((socket) => {
 		} else if (path.startsWith("/files")) {
 			const fileName = path.split("/").slice(2);
 			console.log(process.argv)
-			const dir = process.argv.slice(2).join("/").toString().replace("/", "");
+			const dir = process.argv.slice(3).join("/").toString().replace("/", "");
+			console.log(dir)
 			try {
 				const data = fs.readFileSync(`${dir}/${fileName}`);
 				socket.write(octetStreamResponse(data.toString()));
